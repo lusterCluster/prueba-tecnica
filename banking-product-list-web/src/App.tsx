47e103cb673b8d2d1c8c0dec126a8eee45c3db75
Santi <santi@MacBook-Air-4.local>
2024-08-17 19:07:53 -0500

    initial commit

---
+++ banking-product-list-web/src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+  return (
+    <>
+  
+    <h1 className="text-3xl font-bold underline text-red-800">
+      Hello world!
+    </h1>
+  
+
+    </>
+  )
+}
+
+export default App
